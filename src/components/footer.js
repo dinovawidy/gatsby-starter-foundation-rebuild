ddb29a7575a1c9e19173f1847c9bc7dddce71e6a
Mavin <dinovaprogram@gmail.com>
2020-09-06 00:10:02 +0700

    baru

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,10 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">dinova.my.id</Link></p>
+      <p>Made with <span className="icon -love"><RiHeart2Line/></span> using  <Link to="/">https://www.gatsbyjs.com/</Link></br>Copyright © 2020 Dinova Widytianto. All Rights Reserved</p>
     </div>
   </footer>
 )
+
 
 export default Footer
